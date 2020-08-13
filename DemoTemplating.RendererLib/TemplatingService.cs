c45acd93b7d8f983201859e5def81941710c6cde
Michael Borremans <bom@smart.coop>
2020-08-13 15:33:50 +0200

    Add templating dll

---
+++ DemoTemplating.RendererLib/TemplatingService.cs
@@ -0,0 +1,13 @@
+﻿using RazorEngine;
+using System;
+using RazorEngine.Templating;
+
+namespace DemoTemplating.RendererLib {
+    public class TemplatingService {
+
+        public static string Render(string html, string json) {
+            var data = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+            return Engine.Razor.RunCompile(html, Guid.NewGuid().ToString(), null, data);
+        }
+    }
+}
