fdea2b1fc9fc3df55d114cb763ada95f4bca213f
Michael Borremans <bom@smart.coop>
2020-09-28 15:14:00 +0200

    fix razor engine template compilation taking too much memory. Use async controller and a shared HttpClient instance to increase performances

--- DemoTemplating.Api/Controllers/TemplatingController.cs
+++ DemoTemplating.Api/Controllers/TemplatingController.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace DemoTemplating.Api.Controllers {
     [ApiController]
@@ -10,24 +11,25 @@ namespace DemoTemplating.Api.Controllers {
         private static readonly string TemplateFolder = "./Templates";
         private static readonly string JsonServerUrl = "http://demo-templating-json-server";
 
+        private static HttpClient Client = new HttpClient();
+
         [HttpGet]
         [Route("templating/{htmlFileName}/{jsonFileName}")]
-        public ContentResult Get(string htmlFileName, string jsonFileName) {
+        public async Task<ContentResult> Get(string htmlFileName, string jsonFileName) {
 
             string remoteJsonUrl = $"{JsonServerUrl}/{jsonFileName}.json";
             string json;
 
-            using (var response = new HttpClient().GetAsync(remoteJsonUrl).Result) {
-                if (!response.IsSuccessStatusCode) {
-                    return new ContentResult {
-                        ContentType = "text/html",
-                        StatusCode = (int)HttpStatusCode.NotFound
-                    };
-                }
-
-                json = response.Content.ReadAsStringAsync().Result;
+            var response = await Client.GetAsync(remoteJsonUrl);
+            if (!response.IsSuccessStatusCode) {
+                return new ContentResult {
+                    ContentType = "text/html",
+                    StatusCode = (int)HttpStatusCode.NotFound
+                };
             }
-            
+
+            json = response.Content.ReadAsStringAsync().Result;
+
             string htmlPath = Path.Combine(TemplateFolder, $"{htmlFileName}.html");
             string html = System.IO.File.ReadAllText(htmlPath);
 
